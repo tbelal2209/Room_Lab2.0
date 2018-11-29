1b4532ff1b4a2c37d3ce83286f9d2222e17391ce
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-11-29 08:29:05 -0500

    Create TreasureRoom.java

---
+++ src/Rooms/TreasureRoom.java
@@ -0,0 +1,25 @@
+package Rooms;
+
+import Game.Runner;
+import People.Person;
+
+public class TreasureRoom extends Room {
+    public TreasureRoom(int x, int y) {
+        super(x, y);
+
+    }
+
+    /**
+     * Triggers the game ending conditions.
+     * @param x the Person entering
+     */
+    @Override
+    public void enterRoom(Person x) {
+
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You found the Treasure room! Ten points for Gryffindor.");
+        Runner.gameOff();
+    }
+}
