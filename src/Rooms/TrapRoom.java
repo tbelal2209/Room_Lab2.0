d92b7917955da6958925de952bad0932e1f663e8
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-12-11 08:43:49 -0500

    Update TrapRoom.java

--- src/Rooms/TrapRoom.java
+++ src/Rooms/TrapRoom.java
@@ -3,10 +3,10 @@ package Rooms;
 import Game.Runner;
 import People.Person;
 
-public class WinningRoom extends Room
+public class TrapRoom extends Room
 {
 
-	public WinningRoom(int x, int y) {
+	public TrapRoom(int x, int y) {
 		super(x, y);
 
 	}
@@ -21,8 +21,7 @@ public class WinningRoom extends Room
 		occupant = x;
 		x.setxLoc(this.xLoc);
 		x.setyLoc(this.yLoc);
-		System.out.println("You found the winning room! Ten points for Gryffindor.");
-		Runner.gameOff();
+		System.out.println("Unfortunately you are in the trapped room.");
 	}
 	
 
